647bb8b6a4b452ebd8c3ca1e42b270c07df9933f
GuRX <17633837037@163.com>
2025-04-23 14:38:45 +0800

    检查生成矩阵，发现生成矩阵设置错误导致算法复现失败。

--- src/ccsim.m
+++ src/ccsim.m
@@ -14,12 +14,10 @@ clc; clear; close all;
 
 %************************ Parameters part *************************
 
-nd  = 30000;            % number of data
-cl  = [2 3];            % constraint length
-ber = 0.000;            % bit error rate
-% g1  = 1;              % generator polynomial of 1st
-% g2  = 53;             % generator polynomial of 2st
-% g3  = 137;            % generator polynomial of 3st
+nd    = 30000;            % number of data
+cl    = [2 3];            % constraint length
+poly  = [2 0 3;0 4 5];    % generator polynomial
+ber   = 0.005;            % bit error rate
 
 %*********************** Data generation **************************
 
@@ -27,9 +25,8 @@ data = rand(1, nd) > 0.5;                  % data generation
 
 %******************* Convolutional encodding **********************
 
-genpoly  = [1 0 3;0 1 5];              % generator polynomial
-trellis  = poly2trellis(cl, genpoly);  % arguments of convolutional code 
-codedata = convenc(data, trellis);     % convolutional encoding
+trellis  = poly2trellis(cl, poly);   % arguments of convolutional code 
+codedata = convenc(data, trellis);   % convolutional encoding
 
 %********************** Generate error bits ************************
 
@@ -39,7 +36,7 @@ rcw  = xor(codedata, mask);     % received codeword with errors
 
 %********************* Parameters estimation ***********************
 
-% rcw          = rcw(3:end);
+rcw          = rcw(2:end);
 [n, na]      = cclbr(rcw);          % L : code length
 [t, k, L, H] = ccpbr(rcw, n, na);   % H : check matrix
                                     % t : synchronization position
